626f418c1fa46f5b8ab74072b94defbfa725604c
Wyatt Nolen <wyattnolen@gmail.com>
2021-06-25 00:00:40 -0400

    update

--- src/components/TableSearch.vue
+++ src/components/TableSearch.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="">
-    <h1 style="text-align: center;">Datatable with 3rd Party API</h1>
+    <h1 style="text-align: center">Datatable with 3rd Party API</h1>
     <v-data-table
       :page="page"
-      :items-per-page = "175"
-      
       :headers="headers"
       :items="entries"
+      :items-per-page="175"
       :options.sync="options"
       :server-items-length="totalCards"
       :loading="isLoading"
       class="elevation-1"
     >
-      <template v-slot:item.logo="{ item }">
-        <img :src="item.airline.logo" style="width: 10%;" />
-      </template>
-      <template v-slot:item.website="{ item }">
-        <a :href="item.airline.website">{{ item.airline.website }}</a>
-      </template>
     </v-data-table>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -30,17 +22,14 @@ export default {
     return {
       page: 1,
       totalCards: 0,
-      passengers: [],
       isLoading: true,
       entries: [],
       options: {},
-      search: "*",
-      headers: [
-        { text: 'Name', value: 'name' },
-      ],
+      search: "Goblin",
+      headers: [{ text: "Name", value: "name" }],
     };
   },
-  //this one will populate new data set when user changes current page. 
+  //this one will populate new data set when user changes current page.
   watch: {
     options: {
       handler() {
@@ -50,24 +39,23 @@ export default {
     deep: true,
   },
   methods: {
-    //Reading data from API method. 
+    //Reading data from API method.
     readDataFromAPI() {
       this.isLoading = true;
       const { page } = this.options;
-      let pageNumber = page - 1;
+      let pageNumber = page;
       this.axios
-        .get(`https://api.scryfall.com/cards/search?q=${this.search}&page=${pageNumber}`)
+        .get(
+          `https://api.scryfall.com/cards/search?q=${this.search}&page=${pageNumber}`
+        )
         .then((response) => {
           //Then injecting the result to datatable parameters.
           this.isLoading = false;
           this.entries = response.data.data;
-          this.totalCards = response.data.total_cards
+          this.totalCards = response.data.total_cards;
+          console.log(response);
         });
     },
-  },
-  //this will trigger in the onReady State
-  mounted() {
-    this.readDataFromAPI();
   },
 };
 </script>
